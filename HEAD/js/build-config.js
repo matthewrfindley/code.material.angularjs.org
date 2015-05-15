40daac65c4afa65588b046078b4e125f4fe50f54
Matthew Findley <findley@substantial.com>
2015-05-15 13:09:13 -0700

    snapshot: improved-ux

--- HEAD/js/build-config.js
+++ HEAD/js/build-config.js
@@ -2,6 +2,6 @@ DocsApp.constant('BUILDCONFIG', {
   "ngVersion": "1.3.15",
   "version": "0.9.4",
   "repository": "https://github.com/angular/material",
-  "date": "2015-05-15 08:57:18 -0700",
-  "commit": "0ff4d1a47248f7155de3e7568de25016be70ff8e"
+  "commit": "0ff4d1a47248f7155de3e7568de25016be70ff8e",
+  "date": "2015-05-15 08:57:18 -0700"
 });
